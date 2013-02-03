891dcb30b39edd299b010a899455e3b2d67d57f9
Timothée Boucher <timothee@gmail.com>
2013-02-03 09:50:12 -0800

    Add tests for redirects and CORS headers

--- passeplat_tests.py
+++ passeplat_tests.py
@@ -32,5 +32,30 @@ class PasseplatTestCase(unittest.TestCase):
         response = self.client.get('/status/404')
         assert response.status_code == 404
 
+    def test_redirects(self):
+        response = self.client.get('/redirect/6')
+        assert response.status_code == 200
+
+    def test_cors(self):
+        passeplat.app.config['CORS_DOMAINS'] = "*"
+        response = self.client.get('/get')
+        assert response.headers.get('Access-Control-Allow-Origin') == '*'
+        response = self.client.get('/get', headers={'Origin': 'my.origin'})
+        assert response.headers.get('Access-Control-Allow-Origin') == 'my.origin'
+
+        passeplat.app.config['CORS_DOMAINS'] = "my.origin"
+        response = self.client.get('/get')
+        assert response.headers.get('Access-Control-Allow-Origin') == None
+        response = self.client.get('/get', headers={'Origin': 'wrong.origin'})
+        assert response.headers.get('Access-Control-Allow-Origin') == None
+        response = self.client.get('/get', headers={'Origin': 'my.origin'})
+        assert response.headers.get('Access-Control-Allow-Origin') == 'my.origin'
+
+        response = self.client.open('/', method='OPTIONS')
+        assert response.headers.get('Access-Control-Max-Age')
+        assert response.headers.get('Access-Control-Allow-Credentials') == "true"
+        assert response.headers.get('Access-Control-Allow-Methods') == ', '.join(passeplat.app.config['ALLOWED_HTTP_METHODS'])
+
+
 if __name__ == '__main__':
     unittest.main()
